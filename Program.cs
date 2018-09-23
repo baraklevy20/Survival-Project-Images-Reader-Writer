4bbf0589427a3f8097b4800cb17a38fc6222ec9c
Barak Levy <baraklevy20@gmail.com>
2018-09-23 18:57:43 +0300

    Fixed a few small issues

--- Program.cs
+++ Program.cs
@@ -14,16 +14,12 @@ namespace SurvivalProjectImagePacker
 
         static void Main(string[] args)
         {
-            args = new string[2];
-            args[0] = "-e";
-            args[1] = @"C:\Users\Barak\source\repos\SurvivalProjectImagePacker\SurvivalProjectImagePacker\bin\Debug";
-            //args[1] = @"C:\Users\Barak\source\repos\SurvivalProjectImagePacker\SurvivalProjectImagePacker\bin\Debug\images";
-
             if (args.Length != 2 || (args[0] != "-e" && args[0] != "-p"))
             {
                 Console.WriteLine("Invalid usage. Usage:");
                 Console.WriteLine("-e PATH to extract the PAK file. PATH must contain image.idx and image.pak");
                 Console.WriteLine("-p PATH to pack into a PAK file. PATH needs to be the extracted images folder");
+                return;
                 // Console.WriteLine("-d PATH to decompress a PGF. PATH needs to be the extracted images folder");
                 // Console.WriteLine("-c PATH to compress an image into PGF. PATH needs to be the extracted images folder");
             }
